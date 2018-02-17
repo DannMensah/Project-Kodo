385b6a2b59cb852f0f80c5a0648d7dc2078b15f8
mensahdann <mensahd1@niksula.hut.fi>
2018-02-17 16:58:23 +0200

    Processing for image data done

--- models/autopilot/model.py
+++ models/autopilot/model.py
@@ -1,11 +1,19 @@
 import os
 import numpy as np
-from utilities import (stack_npy_files_in_dir, try_make_dirs)
+from utilities import (stack_npy_files_in_dir, try_make_dirs, resize_and_stack_images_in_dir)
 MODEL_PATH = os.path.dirname(os.path.abspath(__file__))
 MODEL_NAME = "Autopilot"
+
+
 def process(data_folder):
-    y = stack_npy_files_in_dir("{}/key-events".format(data_folder))
+    Y = stack_npy_files_in_dir("{}/key-events".format(data_folder))
     data_folder_name = data_folder.split("/")[-1]
     save_path = "{}/data/{}".format(MODEL_PATH, data_folder_name)
     try_make_dirs(save_path)
-    np.save("{}/y".format(save_path), y)
+    np.save("{}/Y".format(save_path), Y)
+
+    height = 66
+    width = 200
+    depth = 3
+    X = resize_and_stack_images_in_dir("{}/images".format(data_folder), height, width, depth)
+    np.save("{}/X".format(save_path), X)
--- utilities.py
+++ utilities.py
@@ -1,6 +1,7 @@
 import os
 
 import numpy as np
+from skimage.transform import resize
 
 def stack_npy_files_in_dir(directory):
     merged_array = None
@@ -14,9 +15,25 @@ def stack_npy_files_in_dir(directory):
             continue
     return merged_array
 
+
 def try_make_dirs(dir):
     try:
         os.makedirs(dir)
     except FileExistsError:
         pass
+
+def resize_and_stack_images_in_dir(directory, h, w, d):
+    merged_array = None
+    for filename in os.listdir(directory):
+        if filename.endswith(".npy"):
+            if not type(merged_array) is np.ndarray:
+                img = np.load("{}/{}".format(directory, filename))
+                merged_array = resize(img, (h,w,d))
+            else:
+                img = np.load("{}/{}".format(directory, filename))
+                img = resize(img, (h,w,d), mode="reflect")
+                merged_array = np.vstack((merged_array, img))
+        else:
+            continue
+    return merged_array
 
