b666154ea71577a2894aa370c42c79b970ae381c
Dann Mensah <mensah.dann@gmail.com>
2018-02-18 12:23:10 +0200

    Hacky, but should work

--- controllers.py
+++ controllers.py
@@ -1,6 +1,6 @@
 import pyvjoy
 
-from utilities import XBOX_TO_PYVJOY
+from controller_mappings import (XBOX_TO_PYVJOY, PYGAME_TO_XBOX)
 
 class PyvJoyXboxController:
     def __init__(self, data_key_labels, button_threshold=0.5):
@@ -9,21 +9,21 @@ class PyvJoyXboxController:
         self.button_threshold = button_threshold
     
     def scale_axis(self, value):
-        max_vjoy = 32767
+        max_vjoy = 32767.0
         # [-1, 1] to [0, 32767]
-        return ((value + 1) / 2) * 32767
+        return int(((float(value) + 1.0) / 2.0) * max_vjoy)
 
     def threshold_button(self, value):
-        if value >= button_threshold:
+        if value >= self.button_threshold:
             return 1
         return 0
 
     def emit_keys(self, output_values):
         for idx, value in enumerate(output_values):
-            key_label = self.data_key_labels[i]
+            key_label = PYGAME_TO_XBOX[self.data_key_labels[idx]]
             if key_label in XBOX_TO_PYVJOY["AXES"].keys():
                 scaled = self.scale_axis(value)
-                j.set_axis(XBOX_TO_PYVJOY[key_label], scaled)
+                self.controller.set_axis(XBOX_TO_PYVJOY["AXES"][key_label], scaled)
             elif key_label in XBOX_TO_PYVJOY["BUTTONS"].keys():
-                thresholded = threshold_button(value)
-                j.set_button(XBOX_TO_PYVJOY["BUTTONS"], thresholded)
+                thresholded = self.threshold_button(value)
+                self.controller.set_button(XBOX_TO_PYVJOY["BUTTONS"][key_label], thresholded)
