b6f7d174ccda098607849ed1c9e151d33aadb606
mensahdann <mensahd1@niksula.hut.fi>
2018-02-21 20:33:29 +0200

    Adjusting turning dropping magnitude

--- models/autopilot/model.py
+++ models/autopilot/model.py
@@ -140,11 +140,11 @@ class Model(KodoModel):
         return X, y
 
     def turning_dropping_function(self, x):
-        return (1 / ( 1 + math.exp(-15*(x - 0.4))))
+        return (1 / ( 1 + math.exp(-15*(x - 0.4))))     
 
     def img_is_dropped(self, actions):
         transformed_magnitude = self.turning_dropping_function(abs(actions[0]))
-        braking_magnitude = self.turning_dropping_function((actions[2] + 1)/2)
+        braking_magnitude = self.turning_dropping_function((actions[2] + 1)/2 - 0.3)
         if random.random() < transformed_magnitude:
             return False
         elif random.random() < braking_magnitude:
